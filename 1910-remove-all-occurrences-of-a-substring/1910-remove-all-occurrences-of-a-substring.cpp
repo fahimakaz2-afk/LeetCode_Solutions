fd703288d56f40b3ff222e87586fe5b66e47020e
Fahima <fahima2505419@st.jmi.ac.in>
2026-09-12 11:24:00 +0530

    Time: 1 ms (49.21%), Space: 9.4 MB (40.54%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        
-        while(s.find(part) < s.length()){
+
+        while(s.length()>0 && s.find(part) < s.length()){
         int k  = s.find(part);
         s.erase(k, part.length());;
     }
